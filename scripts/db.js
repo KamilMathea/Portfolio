b738a2f42386629314568afde4874727c22026f7
Kamil Mathea <kamilmathe@gmail.com>
2026-07-21 10:29:15 +0200

    refactor: update project database, image paths and modal subtitle

--- scripts/db.js
+++ scripts/db.js
@@ -3,12 +3,13 @@ const projectsData = [
         number: "01",
         title: "Join",
         description: "Task manager inspired by the Kanban system. Create and organize tasks using drag and drop functions, assign users and categories.",
-        image: "./assets/img/projects/join_mockup.jpg",
+        image: "./assets/img/projects/Screendesign_Join.svg",
         tech: [
             { name: "Angular", icon: "./assets/img/frontend_skills_icons/Angular.svg" },
             { name: "TypeScript", icon: "./assets/img/frontend_skills_icons/TypeScript.svg" },
             { name: "HTML", icon: "./assets/img/frontend_skills_icons/HTML.svg" },
-            { name: "CSS", icon: "./assets/img/frontend_skills_icons/CSS.svg" }
+            { name: "CSS", icon: "./assets/img/frontend_skills_icons/CSS.svg" },
+            { name: "Firebase", icon: "./assets/img/frontend_skills_icons/Firebase.svg" }
         ],
         github: "https://github.com/kamilmathea/join",
         live: "https://kamilmathea.de/join"
@@ -17,7 +18,7 @@ const projectsData = [
         number: "02",
         title: "El Pollo Loco",
         description: "A simple jump-and-run game based on object-oriented programming. Help Pepe collect coins and salsa bottles to defeat the giant chicken.",
-        image: "./assets/img/projects/pollo_mockup.jpg",
+        image: "./assets/img/projects/Screendesign_El_Pollo_Loco.svg",
         tech: [
             { name: "HTML", icon: "./assets/img/frontend_skills_icons/HTML.svg" },
             { name: "CSS", icon: "./assets/img/frontend_skills_icons/CSS.svg" },

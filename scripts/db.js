4345819ef1c13d524eb5a200ff867a5dd59c9499
Kamil Mathea <kamilmathe@gmail.com>
2026-07-21 11:35:11 +0200

    style: update modal tech stack icons to green variants and reset color

--- scripts/db.js
+++ scripts/db.js
@@ -5,11 +5,11 @@ const projectsData = [
         description: "Task manager inspired by the Kanban system. Create and organize tasks using drag and drop functions, assign users and categories.",
         image: "./assets/img/projects/Screendesign_Join.svg",
         tech: [
-            { name: "Angular", icon: "./assets/img/frontend_skills_icons/Angular.svg" },
-            { name: "TypeScript", icon: "./assets/img/frontend_skills_icons/TypeScript.svg" },
-            { name: "HTML", icon: "./assets/img/frontend_skills_icons/HTML.svg" },
-            { name: "CSS", icon: "./assets/img/frontend_skills_icons/CSS.svg" },
-            { name: "Firebase", icon: "./assets/img/frontend_skills_icons/Firebase.svg" }
+            { name: "Angular", icon: "./assets/img/projects/Angular_green.svg" },
+            { name: "TypeScript", icon: "./assets/img/projects/TypeScript_green.svg" },
+            { name: "HTML", icon: "./assets/img/projects/HTML_green.svg" },
+            { name: "CSS", icon: "./assets/img/projects/CSS_green.svg" },
+            { name: "Firebase", icon: "./assets/img/projects/Firebase_green.svg" }
         ],
         github: "https://github.com/kamilmathea/join",
         live: "https://kamilmathea.de/join"
@@ -20,9 +20,9 @@ const projectsData = [
         description: "A simple jump-and-run game based on object-oriented programming. Help Pepe collect coins and salsa bottles to defeat the giant chicken.",
         image: "./assets/img/projects/Screendesign_El_Pollo_Loco.svg",
         tech: [
-            { name: "HTML", icon: "./assets/img/frontend_skills_icons/HTML.svg" },
-            { name: "CSS", icon: "./assets/img/frontend_skills_icons/CSS.svg" },
-            { name: "JavaScript", icon: "./assets/img/frontend_skills_icons/JavaScript.svg" }
+            { name: "HTML", icon: "./assets/img/projects/HTML_green.svg" },
+            { name: "CSS", icon: "./assets/img/projects/CSS_green.svg" },
+            { name: "JavaScript", icon: "./assets/img/projects/JavaScript_green.svg" }
         ],
         github: "https://github.com/kamilmathea/el-pollo-loco",
         live: "https://kamilmathea.de/el-pollo-loco"
